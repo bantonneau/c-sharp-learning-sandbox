6e7de07e2a0fe232f3c60a16a4ba972f7008a67b
benos <benantonn@gmail.com>
2023-11-29 18:49:40 -0600

    update helloworld project

--- LearningCSharp/DotNet7/Start/Overview/HelloWorld/Program.cs
+++ LearningCSharp/DotNet7/Start/Overview/HelloWorld/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("What is your name?");
-string str = Console.ReadLine();
-Console.WriteLine("Hello " + str);
+
+using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string str = Console.ReadLine();
+            Console.WriteLine("Hello " + str);
+        }
+    }
+}
--- LearningCSharp/DotNet7/Start/Overview/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ LearningCSharp/DotNet7/Start/Overview/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18d57556783f850ec1bbeca20c2c758724a9aa3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad6fd2bd615e1ba520871a2ec6dc1ed2188f57da")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
