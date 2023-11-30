dd293bd9afbdeb7e62d4c5a8d1cebc74451b8727
benos <benantonn@gmail.com>
2023-11-29 18:51:08 -0600

    remove boilerplate

--- LearningCSharp/DotNet7/Start/Overview/HelloWorld/Program.cs
+++ LearningCSharp/DotNet7/Start/Overview/HelloWorld/Program.cs
@@ -1,17 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Console.WriteLine("Hello " + str);
 
-namespace MyConsoleApp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("What is your name?");
-            string str = Console.ReadLine();
-            Console.WriteLine("Hello " + str);
-        }
-    }
-}
