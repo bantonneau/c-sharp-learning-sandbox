7289d06664e3962956670ee7125c9f7df80e14c9
benos <benantonn@gmail.com>
2023-12-04 19:57:49 -0600

    c# comment work

--- LearningCSharp/DotNet7/Start/Overview/Comments/Program.cs
+++ LearningCSharp/DotNet7/Start/Overview/Comments/Program.cs
@@ -5,12 +5,20 @@ namespace Comments
     class Program
     {
         /// XML Comments are used to help provide documentation
-        /// They start with triple-slashes and have a special syntax
+        /// They start with triple-slashes and have a special syntax <summary>
+                /// XML Comments are used to help provide documentation
+                /// </summary>
+                /// <param name="args">An array of string arguments from the command line</param>        
         static void Main(string[] args)
         {
-
+            // Comment test
+            // single line comment
+            // eeee
             Console.WriteLine("Hello World!");
-
+            /* multiple line
+            so useful
+            wow
+            */
         }
     }
 }
